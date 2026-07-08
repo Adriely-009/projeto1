1b09b3fe92dcd56281dd99add3d6db520f550957
Adriely-009 <caldeira.oliveira@escola.pr.gov.br>
2026-07-08 09:40:13 -0300

    ...

--- aula01/script.js
+++ aula01/script.js
@@ -3,3 +3,8 @@ function botao(){
     let nt = prompt(`Olá ${oi}, qual sua nota para nosso site :`)
     alert("Agradeço pelo seu feedback")
 }
+
+function botao2(){
+    let email = prompt("Digite seu E-mail")
+    
+}
